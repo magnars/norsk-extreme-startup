6f89cc682120e9c7c0ee0c345546c86c8ddde99a
Johannes Brodwall <johannes@brodwall.com>
2011-08-09 22:25:21 +0200

    All questions require reference to Player

--- spec/extreme_startup/general_knowledge_question_spec.rb
+++ spec/extreme_startup/general_knowledge_question_spec.rb
@@ -3,7 +3,7 @@ require 'extreme_startup/question_factory'
 
 module ExtremeStartup
   describe GeneralKnowledgeQuestion do
-    let(:question) { GeneralKnowledgeQuestion.new }
+    let(:question) { GeneralKnowledgeQuestion.new(Player.new) }
     
     it "converts to a string" do
       question.as_text.should =~ /wh.+/
